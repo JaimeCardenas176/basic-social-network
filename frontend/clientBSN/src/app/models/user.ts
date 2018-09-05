c7f2443ae159efd452d799d1227b607c8ae45a83
JaimeCardenas176 <jaime.cardenas176@gmail.com>
2018-09-05 04:31:54 +0200

    frontend con angular 6 en proceso, creando modelos y un par de componentes

---
+++ frontend/clientBSN/src/app/models/user.ts
@@ -0,0 +1,21 @@
+export class User {
+	public _id: string;
+	public name: string;
+	public surname: string;
+	public email: string;
+	public nick: string;
+	public password: string;
+	public role: string;
+
+	constructor(_id: string, name: string, surname: string, email: string,
+	 nick: string, password: string, role: string){
+	 	this._id = _id;
+	 	this.name = name;
+	 	this.surname = surname;
+	 	this.email = email;
+	 	this.nick= nick,
+	 	this.password = password;
+	 	this.role = role
+	}
+
+}
